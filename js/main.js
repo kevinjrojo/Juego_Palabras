0cea6f0fa69d4a8d30fc2cf9ac578a9e349c4079
kevin <kevin.rojokevin17@gmail.com>
2024-07-15 19:52:41 -0300

    js avanzando

--- js/main.js
+++ js/main.js
@@ -1,19 +1,58 @@
 import { palabrasOrdenadas, palabrasDesordenadas } from "./palabras.js";
 
-function ObtenerPalabras() {
-  Math.random();
+let palabras = document.querySelector(".palabra");
+let inputs = document.querySelector(".respuestas");
 
-  for (let i = 0; i < palabrasDesordenadas.length - 1; i++) {
-    let palabrasdes = palabrasDesordenadas[0].desordenado2;
-    console.log(palabrasdes);
+for (let i = 0; i < palabrasDesordenadas.length; i++) {
+  let palabrasdes = palabrasDesordenadas[i];
+  let valor = Object.keys(palabrasdes);
+  let indiceAleatorio = Math.floor(Math.random() * valor.length);
+  let valorAleatorio = valor[indiceAleatorio];
+  let palabraAleatorio = palabrasdes[valorAleatorio];
+  palabras.innerText = palabraAleatorio;
+  for (let i = 0; i < palabraAleatorio.length; i++) {
+    let input = document.createElement("input");
+    input.type = "text";
+    input.classList.add("letra");
+    input.pattern = "[A-Za-z]";
+    input.maxLength = 1;
+    inputs.appendChild(input);
   }
-  for (let i = 0; i < palabrasOrdenadas.length - 1; i++) {
-    const palabrasord = palabrasOrdenadas[i];
-    console.log(palabrasord);
-  }
+
+  let inp = document.querySelectorAll('input[type="text"]');
+  inp[0].focus();
+
+  inp.forEach((input, index) => {
+    //  aca hay misma cantidad de input y index
+    input.addEventListener("keyup", function (event) {
+      if (event.key.length === 1 && index < inp.length - 1) {
+        // se cumple porque es -1
+        inp[index + 1].focus();
+        respuestaCorrecta.push({
+          key: event.key,
+        });
+      }
+    });
+  });
 }
 
-ObtenerPalabras();
-//let aletorio = Math.floor(Math.random() * nombre.length);
+let respuestaCorrecta = [];
+
+setTimeout(() => {
+  for (let i = 0; i < respuestaCorrecta.length; i++) {
+    const element = respuestaCorrecta[i].key;
+    console.log(element);
+  }
+}, 10000);
+
+/*for (let i = 0; i < palabrasOrdenadas.length; i++) {
+  let palabras = palabrasOrdenadas[i];
+
+  let aletorio = Math.random(palabras);
+  let palabrasAletorias = palabras[aletorio];
+
+  console.log(palabrasAletorias);
+}*/
+
 //console.log(palabras[i].ordenado + palabras[i].desordenado);
 //console.log(palabras[0]);
--- js/palabras.js
+++ js/palabras.js
@@ -1,15 +1,8 @@
-export const palabrasOrdenadas = ["hola", "dinero"];
+export const palabrasOrdenadas = ["hola", "dinero", "ayuda"];
 export const palabrasDesordenadas = [
   {
     desordenado1: "lhao",
-    desordenado2: "alho",
-    desordenado3: "ohal",
-    desordenado4: "loha",
-  },
-  {
-    desordenado1: "nediro",
     desordenado2: "ronedi",
-    desordenado3: "nidero",
-    desordenado4: "noderi",
+    desordenado3: "duaya",
   },
 ];
